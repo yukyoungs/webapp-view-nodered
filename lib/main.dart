c9524884caceab1b655d42a98b1a196f2c035833
yukyoungs <syk9848@gmail.com>
2025-04-08 17:24:36 +0900

    debug delete

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Node-RED Dashboard Viewer',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
